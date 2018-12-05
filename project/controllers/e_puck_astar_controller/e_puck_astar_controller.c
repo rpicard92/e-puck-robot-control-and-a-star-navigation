4a670bd472096c0ee1b311b47f6dfa19f8e68fd5
ron <rpicard92@gmail.com>
2018-12-05 07:49:25 -0600

    built a command structure for the epuck

--- project/controllers/e_puck_astar_controller/e_puck_astar_controller.c
+++ project/controllers/e_puck_astar_controller/e_puck_astar_controller.c
@@ -53,206 +53,364 @@
 #define GS_LEFT 0
 #define GS_CENTER 1
 #define GS_RIGHT 2
-WbDeviceTag gs[NB_GROUND_SENS]; /* ground sensors */
+
+// ground sensors
+WbDeviceTag gs[NB_GROUND_SENS];
 unsigned short gs_value[NB_GROUND_SENS]={0,0,0};
 
-// LEDs
-#define NB_LEDS    8
-WbDeviceTag led[NB_LEDS];
-
-//------------------------------------------------------------------------------
-//
-//    BEHAVIORAL MODULES
-//
-//------------------------------------------------------------------------------
-
-////////////////////////////////////////////
-// LFM - Line Following Module
-//
-// This module implements a very simple, Braitenberg-like behavior in order
-// to follow a black line on the ground. Output speeds are stored in
-// lfm_speed[LEFT] and lfm_speed[RIGHT].
-
+// grid
+unsigned short map[12][12] = {
+    {0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,1,0,0,0,0,0,0,0,0,0,0},
+    {0,1,0,0,0,0,0,0,0,0,0,0},
+    {0,1,0,0,0,0,0,0,0,0,0,0},
+    {0,1,0,0,0,0,0,0,0,0,0,0},
+    {0,1,0,0,0,0,0,0,0,0,0,0},
+    {0,1,0,0,0,0,0,0,0,0,0,0},
+    {0,1,0,0,0,0,0,0,0,0,0,0},
+    {0,1,0,0,0,0,0,0,0,0,0,0},
+    {0,1,0,0,0,0,0,0,0,0,0,0},
+    {0,1,1,1,1,1,1,1,1,1,1,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0}
+  };
+// wheel speeds
 int lfm_speed[2];
 
+// epuck wheel speed
 #define LFM_FORWARD_SPEED 200
+
+// gain for line following steering correction
 #define LFM_K_GS_SPEED 0.1
 
-
-/* Threasholds are used as cutoff values for detecting
-   the track and the board. -RonPicard
-*/
+// Threasholds are used as cutoff values for detecting the track and the board. -RonPicard
 #define Board_Min_Threshold 700
 #define Track_Max_Threshold 500
 
-/*  Keep track of # of nodes detected -RonPicard
-*/
+// Keep track of # of nodes detected -RonPicard
 int nodeCount = 0;
 int stillOnCurrentNode = 0;
 int turningLock = 0;
 int turningTimer = 0;
+int turnInit = 0;
+int i;
+int speed[2];
+int Mode=1;
 
-/* This function tells whether a censor is reading a line, a board, or neither -RonPicard
-*/
+
+// this function tells whether a sensor is detects a line
 int isLineSensed(int GroundSensorValue){
-  // If the sensor detects a line
+  // if the sensor detects a line
   if(GroundSensorValue<=Track_Max_Threshold){
-    // on line
     return 1;
-  // If the sensor does not detect line
+  // if the sensor does not detect line
   } else if(GroundSensorValue>=Board_Min_Threshold){
-    // not on line
     return 0;
+  // if unknown
   } else {
-    // unknown
     return -1;
   }
 }
 
-/* This function tests whether the robot is at on a node, line, or unknown -RonPicard
-*/
-int isNode(int left, int center, int right){
-  // If sensor all sensors a line
+// this function tells whether the robot is on a node
+int isNodeSensed(int left, int center, int right){
+  // if all sensors are on line
   if((left == 1) && (center == 1) && (right == 1)){
-    // on node
     return 1;
-  } else {
-    // not on node
-    return 0;
   }
+  // not on a node
+  return 0;
 }
 
 
-// Detected when you pass a node -RonPicard
+// this function tells whether the robot detects a new node
 int detectNode(int left, int center, int right){
-  // If the logic does not think it is on a node
+  // if the robot has left the nodenode
   if (stillOnCurrentNode == 0){
-    // Find out if the robot is still on a node
-    int node = isNode(left, center, right);
-    // If the robot is on a node
+    // find out if the robot senses a new node
+    int node = isNodeSensed(left, center, right);
+    // if the robot is senses a new node
     if(node == 1){
-      // Add node to count
+      // add the node to count
       nodeCount = nodeCount + 1;
-      // Change to 1 since the robot is on the node
+      // mark that the robot is on a new node
       stillOnCurrentNode = 1;
+      // logger
       printf("\nNode # %d Found\n",nodeCount);
       return 1;
     }
   }
+  // the robot is still on the current node
   return 0;
 }
 
-// Detect when the rebot has just passed a node
+// this function detects when the robot has just left a node
 int detectLineJustPassedNode(int left, int center, int right){
- // If the logic still thinks it's on current node
+ // if the robot has been on a node
  if(stillOnCurrentNode == 1){
-    // Find out if the robot is still on a node
-    int node = isNode(left, center, right);
-    // If the robot is not still on a node
+    // find out if the robot is still on the node
+    int node = isNodeSensed(left, center, right);
+    // if the robot is not still on the node
     if(node == 0){
-      printf("\nBack On Line\n");
-      // Change to 0 since the robot is not on the node
+      // mark that the robot is no longer on the node
       stillOnCurrentNode = 0;
+      // logger
+      printf("\nBack On Line\n");
       return 1;
     }
   }
   return 0;
 };
 
-// This function contain the turn logic -RonPicard
+// this function contain the turn logic
 void turn(int turnDirection, int LeftSensorOnTrack, int CenterSensorOnTrack, int RightSensorOnTrack){
-    // If not locked for turning
-    if(turningLock==0){
-      // Lock for turning
+    // if the robot is not currently turning
+  if(turningLock==0){
+      // mark that the robot is turning
       turningLock = 1;
-      // Set intial turn timer to 25
+      // set a timer for 25 seconds. this will allow the robot get completely off a line before attempting to
+      // sense the line it is tuning onto.
       turningTimer = 25;
-      // If this is a left turn
-      if(turnDirection==0){
-        // Hard coded turning contants
-        lfm_speed[LEFT]  = -40;
-        lfm_speed[RIGHT] = 180;
-        printf("\nLEFT TURN START\n");
-      // If this is right turn
-      } else if(turnDirection==1){
-        // Hard coded turning contants
-        lfm_speed[LEFT]  = 180;
-        lfm_speed[RIGHT] = -40;
-        printf("\nRIGHT TURN START\n");
-      }
-    // If turn is locked
-    } else if(turningLock==1){
-      // If initial turn timer is not 0
-      if(turningTimer !=0){
-        // decrement timer
-        turningTimer = turningTimer - 1;
-        //printf("\nTIMER: %d ",timer);
-      }
-      // If timer is finished
-      if(turningTimer == 0){
-        // For Left turn - If right and center sensor detect the line
-        if((turnDirection == 0) && (LeftSensorOnTrack==0) && (CenterSensorOnTrack==1) && (RightSensorOnTrack == 1)){
-          // Unlock turn
-          turningLock = 0;
-          printf("\nLEFT TURN FINISHED\n");
-        // For Right turn - If left and center sensor detect the line
-        } else if((turnDirection == 1) && (LeftSensorOnTrack==0) && (CenterSensorOnTrack==1) && (RightSensorOnTrack == 1)){
-          turningLock = 0;
-          printf("\nRIGHT TURN FINISHED\n");
-        }
-       }
+    // if the robot is turning left
+    if(turnDirection==0){
+      // hard coded turning constants
+      lfm_speed[LEFT]  = -70;
+      lfm_speed[RIGHT] = 180;
+      // logger
+      printf("\nLEFT TURN START\n");
+      // if the robot is turning right
+    } else if(turnDirection==1){
+      // hard coded turning constants
+      lfm_speed[LEFT]  = 180;
+      lfm_speed[RIGHT] = -70;
+      // logger
+      printf("\nRIGHT TURN START\n");
     }
+  // if the robot is still turning
+  } else if(turningLock==1){
+    // if turn timer has not reached 0
+    if(turningTimer !=0){
+      // decrement the timer
+      turningTimer = turningTimer - 1;
+    }
+    // if turn timer has reached 0
+    if(turningTimer == 0){
+      // if a left turn has been completed
+      if((turnDirection == 0) && (LeftSensorOnTrack==0) && (CenterSensorOnTrack==1) && (RightSensorOnTrack == 1)){
+        // mark that the robot is no longer turning
+        turningLock = 0;
+        // logger
+        printf("\nLEFT TURN FINISHED\n");
+      // if a right turn has been completed
+      } else if((turnDirection == 1) && (LeftSensorOnTrack==1) && (CenterSensorOnTrack==1) && (RightSensorOnTrack == 0)){
+        // mark that the robot is no longer turning
+        turningLock = 0;
+        // logger
+        printf("\nRIGHT TURN FINISHED\n");
+      }
+    }
+  }
 }
 
-/* This function adjust the driving logic -RonPicard
-*/
-void navigate(void)
-{
-  // Get sensor values
+void driveStraight(int numberOfNodes){
+  // loop till the robot has passed the number of nodes
+  if(nodeCount<=numberOfNodes){
+    // get the ground sensor values
+    int LeftSensorReading = gs_value[GS_LEFT];
+    int CenterSensorReading = gs_value[GS_CENTER];
+    int RightSensorReading = gs_value[GS_RIGHT];
+    // logger
+    //printf("\nLEFT: %d, Center: %d, Right %d",LeftSensorReading,CenterSensorReading,RightSensorReading);
+    
+    // find out which sensors are on the line
+    int LeftSensorOnTrack = isLineSensed(LeftSensorReading);
+    int CenterSensorOnTrack = isLineSensed(CenterSensorReading);
+    int RightSensorOnTrack = isLineSensed(RightSensorReading);
+    // logger
+    // printf("\nLEFT: %d, Center: %d, Right %d",LeftSensorOnTrack,CenterSensorOnTrack,RightSensorOnTrack);
+    
+    // find out if robot is detects a new node
+    int nodeDetected = detectNode(LeftSensorOnTrack, CenterSensorOnTrack, RightSensorOnTrack);
+    // logger
+    // printf("\Node: %d, nodeDetected);
+  
+    // find out if the robot has just passed the node
+    int lineJustPassedNodeDetected = detectLineJustPassedNode(LeftSensorOnTrack,CenterSensorOnTrack,RightSensorOnTrack);
+    // logger
+    // printf("\JustPassedNode: %d, lineJustPassedNodeDetected);
+
+    // stay on the line (Braitenberg-like behavior)
+    int DeltaS=0;
+    DeltaS = RightSensorReading-LeftSensorReading;
+    lfm_speed[LEFT]  = LFM_FORWARD_SPEED - LFM_K_GS_SPEED * DeltaS;
+    lfm_speed[RIGHT] = LFM_FORWARD_SPEED + LFM_K_GS_SPEED * DeltaS;
+  } else {
+    lfm_speed[LEFT]=0;
+    lfm_speed[RIGHT]=0;
+  }
+}
+
+void leftTurn(){
+  // get the ground sensor values
   int LeftSensorReading = gs_value[GS_LEFT];
   int CenterSensorReading = gs_value[GS_CENTER];
   int RightSensorReading = gs_value[GS_RIGHT];
-  
-  //Log Sensor values -RonPicard
+  // logger
   //printf("\nLEFT: %d, Center: %d, Right %d",LeftSensorReading,CenterSensorReading,RightSensorReading);
   
-  // Find out if sensors detect the track
+  // find out which sensors are on the line
   int LeftSensorOnTrack = isLineSensed(LeftSensorReading);
   int CenterSensorOnTrack = isLineSensed(CenterSensorReading);
   int RightSensorOnTrack = isLineSensed(RightSensorReading);
+  // logger
+  // printf("\nLEFT: %d, Center: %d, Right %d",LeftSensorOnTrack,CenterSensorOnTrack,RightSensorOnTrack);
+    
+  // find out if robot is detects a new node
+  int nodeDetected = detectNode(LeftSensorOnTrack, CenterSensorOnTrack, RightSensorOnTrack);
+  // logger
+  // printf("\Node: %d, nodeDetected);
   
-  // 1 if on line, 0 if not on line, -1 if unknown. -RonPicard
+  // find out if the robot has just passed the node
+  int lineJustPassedNodeDetected = detectLineJustPassedNode(LeftSensorOnTrack,CenterSensorOnTrack,RightSensorOnTrack);
+    
+  // loop till the robot has passed the number of nodes
+  if((lineJustPassedNodeDetected == 1) || (turningLock == 1)){
+    // logger
+    // printf("\JustPassedNode: %d, lineJustPassedNodeDetected);
+    // turn the robot
+    turn(0,LeftSensorOnTrack,CenterSensorOnTrack,RightSensorOnTrack);
+    turnInit=1;
+  } else {
+    // stay on the line (Braitenberg-like behavior)
+    int DeltaS=0;
+    DeltaS = RightSensorReading-LeftSensorReading;
+    lfm_speed[LEFT]  = LFM_FORWARD_SPEED - LFM_K_GS_SPEED * DeltaS;
+    lfm_speed[RIGHT] = LFM_FORWARD_SPEED + LFM_K_GS_SPEED * DeltaS;
+  }
+}
+
+void rightTurn(){
+  // get the ground sensor values
+  int LeftSensorReading = gs_value[GS_LEFT];
+  int CenterSensorReading = gs_value[GS_CENTER];
+  int RightSensorReading = gs_value[GS_RIGHT];
+  // logger
+  //printf("\nLEFT: %d, Center: %d, Right %d",LeftSensorReading,CenterSensorReading,RightSensorReading);
+  
+  // find out which sensors are on the line
+  int LeftSensorOnTrack = isLineSensed(LeftSensorReading);
+  int CenterSensorOnTrack = isLineSensed(CenterSensorReading);
+  int RightSensorOnTrack = isLineSensed(RightSensorReading);
+  // logger
+  // printf("\nLEFT: %d, Center: %d, Right %d",LeftSensorOnTrack,CenterSensorOnTrack,RightSensorOnTrack);
+    
+  // find out if robot is detects a new node
+  int nodeDetected = detectNode(LeftSensorOnTrack, CenterSensorOnTrack, RightSensorOnTrack);
+  // logger
+  // printf("\Node: %d, nodeDetected);
+  
+  // find out if the robot has just passed the node
+  int lineJustPassedNodeDetected = detectLineJustPassedNode(LeftSensorOnTrack,CenterSensorOnTrack,RightSensorOnTrack);
+    
+  // loop till the robot has passed the number of nodes
+  if((lineJustPassedNodeDetected == 1) || (turningLock == 1)){
+    // logger
+    // printf("\JustPassedNode: %d, lineJustPassedNodeDetected);
+    // turn the robot
+    turn(1,LeftSensorOnTrack,CenterSensorOnTrack,RightSensorOnTrack);
+    turnInit=1;
+  } else {
+    // stay on the line (Braitenberg-like behavior)
+    int DeltaS=0;
+    DeltaS = RightSensorReading-LeftSensorReading;
+    lfm_speed[LEFT]  = LFM_FORWARD_SPEED - LFM_K_GS_SPEED * DeltaS;
+    lfm_speed[RIGHT] = LFM_FORWARD_SPEED + LFM_K_GS_SPEED * DeltaS;
+  }
+}
+
+void navigate(int numberOfNodes,int direction){
+
+  while(nodeCount<=numberOfNodes){
+    // run one simulation step
+    wb_robot_step(TIME_STEP);
+    // read sensors value
+    for(i=0;i<NB_GROUND_SENS;i++) gs_value[i] = wb_distance_sensor_get_value(gs[i]);
+  
+    if(direction == 2){
+      driveStraight(numberOfNodes);
+    } else if(direction == 0){
+      if((turnInit==0)||(turningLock==1)){
+        leftTurn();
+      } else if((turnInit==1)||(turningLock==0)){
+        driveStraight(numberOfNodes);
+      }
+    } else if(direction == 1){
+      if((turnInit==0)||(turningLock==1)){
+        rightTurn();
+      } else if((turnInit==1)||(turningLock==0)){
+        driveStraight(numberOfNodes);
+      }
+    }
+    wb_differential_wheels_set_speed(lfm_speed[LEFT],lfm_speed[RIGHT]);
+  }
+  printf("/nFINISHED: %d nodes, %d direction",numberOfNodes,direction);
+  turnInit=0;
+  nodeCount=0;
+  wb_differential_wheels_set_speed(0,0);
+}
+
+
+// this function adjust the driving logic
+void drive()
+{
+  // get the ground sensor values
+  int LeftSensorReading = gs_value[GS_LEFT];
+  int CenterSensorReading = gs_value[GS_CENTER];
+  int RightSensorReading = gs_value[GS_RIGHT];
+  // logger
+  //printf("\nLEFT: %d, Center: %d, Right %d",LeftSensorReading,CenterSensorReading,RightSensorReading);
+  
+  // find out which sensors are on the line
+  int LeftSensorOnTrack = isLineSensed(LeftSensorReading);
+  int CenterSensorOnTrack = isLineSensed(CenterSensorReading);
+  int RightSensorOnTrack = isLineSensed(RightSensorReading);
+  // logger
   // printf("\nLEFT: %d, Center: %d, Right %d",LeftSensorOnTrack,CenterSensorOnTrack,RightSensorOnTrack);
   
-  // Find out if the robot is on a node
+  // find out if robot is detects a new node
   int nodeDetected = detectNode(LeftSensorOnTrack, CenterSensorOnTrack, RightSensorOnTrack);
-  
-  // Find out if the robot just passed a node
+  // logger
+  // printf("\Node: %d, nodeDetected);
+
+  // find out if the robot has just passed the node
   int lineJustPassedNodeDetected = detectLineJustPassedNode(LeftSensorOnTrack,CenterSensorOnTrack,RightSensorOnTrack);
+  // logger
+  // printf("\JustPassedNode: %d, lineJustPassedNodeDetected);
+
+  // hardcode turn direction (temporary)
+  int turnDirection = 0;
   
-  // Hardcode turn direction (temporary)
-  int turnDirection = 1;
-  
-  // If node is detected and the robot is not turnning
+  // if the robot has just entered a node and the robot is not currently turning
   if((nodeDetected == 1) && (turningLock == 0)){
-    // Continue straight
+    // drive straight
     lfm_speed[LEFT]  = LFM_FORWARD_SPEED;
     lfm_speed[RIGHT] = LFM_FORWARD_SPEED;
-  // If you the robot just passed a not
-  } else if((lineJustPassedNodeDetected == 1) || (turningLock ==1)){
-       // Turn the robot
-       turn(turnDirection,LeftSensorOnTrack,CenterSensorOnTrack,RightSensorOnTrack);
-  // If the robot is not locked for turning
+  // if the robot just passed a node or the the robot is currently turning
+  }else if((lineJustPassedNodeDetected == 1) || (turningLock == 1)){
+     // turn the robot
+     turn(turnDirection,LeftSensorOnTrack,CenterSensorOnTrack,RightSensorOnTrack);
+  // if the robot is not turning
   }else if(turningLock == 0){
-    // Stay on the line
+    // stay on the line (Braitenberg-like behavior)
     int DeltaS=0;
     DeltaS = RightSensorReading-LeftSensorReading;
     lfm_speed[LEFT]  = LFM_FORWARD_SPEED - LFM_K_GS_SPEED * DeltaS;
     lfm_speed[RIGHT] = LFM_FORWARD_SPEED + LFM_K_GS_SPEED * DeltaS;
   }
   
+  
+  
+  
 }
-
 
 
 //------------------------------------------------------------------------------
@@ -265,27 +423,18 @@ void navigate(void)
 // Main
 int main()
 {
-  int i, speed[2], Mode=1;
 
   /* intialize Webots */
   wb_robot_init();
 
   /* initialization */
   char name[20];
-  for (i = 0; i < NB_LEDS; i++) {
-    sprintf(name, "led%d", i);
-    led[i] = wb_robot_get_device(name); /* get a handler to the sensor */
-  }
+
   for (i = 0; i < NB_GROUND_SENS; i++) {
     sprintf(name, "gs%d", i);
     gs[i] = wb_robot_get_device(name); /* ground sensors */
     wb_distance_sensor_enable(gs[i],TIME_STEP);
   }
-
-  for(;;)   // Main loop
-  {
-    // Run one simulation step
-    wb_robot_step(TIME_STEP);
 
     // Reset all BB variables when switching from simulation to real robot and back
     if (Mode!=wb_robot_get_mode())
@@ -300,9 +449,6 @@ int main()
       }
     }
 
-    // read sensors value
-    for(i=0;i<NB_GROUND_SENS;i++) gs_value[i] = wb_distance_sensor_get_value(gs[i]);
-
     // Reset all BB variables when switching from simulation to real robot and back
     if (Mode!=wb_robot_get_mode())
     {
@@ -311,22 +457,12 @@ int main()
       else if (Mode == REALITY) printf("\nSwitching to REALITY and reseting all BB variables.\n\n");
     }
 
-    // Speed initialization
     speed[LEFT] = 0;
     speed[RIGHT] = 0;
-
     // Navigation logic
-    navigate();
-
-    // Speed computation
-    speed[LEFT]  = lfm_speed[LEFT];
-    speed[RIGHT] = lfm_speed[RIGHT];
-
-
-    // Set wheel speeds
-    //wb_differential_wheels_set_speed(speed[LEFT],speed[RIGHT]);
-    wb_differential_wheels_set_speed(speed[LEFT],speed[RIGHT]);
-
-  }
+    navigate(2,2);
+    navigate(3,0);
+    navigate(5,1);
+    //drive();
   return 0;
 }
